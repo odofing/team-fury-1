332c0ecbc5a46a9496c289f76031e0c2d5edb6d3
odofing <odofing@gmail.com>
2020-06-03 15:46:22 +0100

    On branch lekan 	modified:   scripts/gbadamosi_odofin_00635.js

--- Documents/HNGi7_Titans_Task2-master/scripts/gbadamosi_odofin_00635.js
+++ Documents/HNGi7_Titans_Task2-master/scripts/gbadamosi_odofin_00635.js
@@ -1,4 +1,6 @@
-const project = ["Odofin Gbadamosi", "00635", "Javascript", "odofing@gmail.com"];
-[arr1, arr2, arr3, arr4] = project;
+const projects = ["Odofin Gbadamosi", "00635", "Javascript", "odofing@gmail.com"];
+[arr1, arr2, arr3, arr4] = projects;
+
+
 
  console.log(`Hello World, this is ${arr1} with ${arr2} with email ${arr4} using ${arr3} for stage 2 task`);
